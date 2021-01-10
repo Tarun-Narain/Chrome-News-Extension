bb4f21c9c2e7d52e17d6bbc810a8be947746aa93
Tarun Narain <TarunNarain@fico.com>
2021-01-10 12:44:26 +0530

    Bearer Token Expired

--- background.js
+++ background.js
@@ -4,7 +4,7 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
         url: 'https://newsapi.org/v2/everything?q=ransomware&language=en&pageSize=5&page='+request.pageNo,
         method: 'GET',
         beforeSend: function (xhr) {
-            xhr.setRequestHeader('Authorization', 'Bearer 8de946b14433428eb8ed38407ff95006');
+            xhr.setRequestHeader('Authorization', 'Bearer ***');
         },
         success: function(data) {
             // data would be sent successfully
